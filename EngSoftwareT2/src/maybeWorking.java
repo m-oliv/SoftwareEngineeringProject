2fcf205c62558c843e15d7a214dcd89725e211c4
Cloudp <joaiveca@gmail.com>
2014-05-08 19:48:43 +0100

    bdskfjgbwsjkdfb

--- EngSoftwareT2/src/maybeWorking.java
+++ EngSoftwareT2/src/maybeWorking.java
@@ -1,17 +1,4 @@
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-
- 
-
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(maybeWorking.class)
 
 public class maybeWorking {
 
-	public static void main(String[]args){
-	
-		
-	}
-	
-	
 }
