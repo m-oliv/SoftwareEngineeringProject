dc2f8f2772738d952b1bc0de84aa1a43226d2eda
Marlene Oliveira <marleneoliveira7@gmail.com>
2014-06-14 19:06:53 +0100

    organizaçao certa. removido lixo

--- EngSoftwareT2/src/Documento.java
+++ EngSoftwareT2/src/Documento.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.Timestamp;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -10,6 +13,9 @@ public class Documento {
 	private Timestamp d_criacao;
 	private Timestamp d_alteracao;
 	private int id_user = -1;
+	
+	private String readValue=null;
+	private File documentFile;
 
 	public Documento(int id, String title, String body, Timestamp timestamp, int id_user) throws Exception {
 		if (title.equals(null)) {
@@ -18,7 +24,7 @@ public class Documento {
 		if (body.equals(null)) {
 			throw new NullPointerException();
 		}
-		if (id_user == -1) {
+		if (id_user == -1) { 
 			throw new Exception("id_user wrong");
 		}
 		if (timestamp.equals(null)) {
@@ -29,6 +35,59 @@ public class Documento {
 		this.body = body;
 		this.d_criacao = timestamp;
 		this.id_user = id_user;
+	}
+	
+	/*OVERLOAD: Versão que carrega com filepath em vez de hard coded title e body*/
+	public Documento(int id, String filepath, Timestamp timestamp, int id_user) throws Exception {
+		if (id_user == -1) {
+			throw new Exception("id_user wrong");
+		}
+		if (timestamp.equals(null)) {
+			throw new NullPointerException();
+		}
+		this.id = id;
+		this.d_criacao = timestamp;
+		this.id_user = id_user;	
+		documentFile = new File(filepath);
+		BufferedReader reader = null;
+		try 
+		{
+			reader = new BufferedReader (new FileReader(documentFile) );
+			// Linha 1: "title:"
+			readValue=reader.readLine();
+			// Linha 2: "sometitle"
+			readValue=reader.readLine();
+			title=readValue;
+			readValue=reader.readLine();
+			// Linha 4: "body:"
+			readValue=reader.readLine();
+			// Linhas 5+: "anything goes"
+			readValue=reader.readLine();
+			body="";
+			while(readValue!=null)
+			{
+				body+=readValue+"\n";
+				readValue=reader.readLine();
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally 
+		{
+		    try 
+		    {
+		        if (reader != null) 
+		        {
+		            reader.close();
+		        }
+		    } 
+		    catch (Exception e) 
+		    {
+				e.printStackTrace();
+		    }
+		}
+		
 	}
 
 	public String getTitle() {
@@ -152,43 +211,43 @@ public class Documento {
 		}
 
 	}
-	
+
 	public void updateDocBody(DBAccess dba, String n, int id, Timestamp d_alteracao) throws SQLException {
 
-	
+
 		Connection conn = dba.getConnection();
 		Statement stmt = conn.createStatement();
-		
-			if (n.equals(null)) {
-				throw new NullPointerException();
-			} else {
-				String sqlQuery = "UPDATE documentos SET body = '" + n
-						+ "' WHERE id = " + id;
-				stmt.execute(sqlQuery);
-				String sqlQuerytime_update = "UPDATE documentos SET d_alteracao = '"
-						+ d_alteracao + "' WHERE id = " + id;
-				stmt.execute(sqlQuerytime_update);
-			}
-		
+
+		if (n.equals(null)) {
+			throw new NullPointerException();
+		} else {
+			String sqlQuery = "UPDATE documentos SET body = '" + n
+					+ "' WHERE id = " + id;
+			stmt.execute(sqlQuery);
+			String sqlQuerytime_update = "UPDATE documentos SET d_alteracao = '"
+					+ d_alteracao + "' WHERE id = " + id;
+			stmt.execute(sqlQuerytime_update);
+		}
+
 	}
-	
+
 	public void updateDocId_user(DBAccess dba, int id_user,	int id, Timestamp d_alteracao) throws Exception {
 
 		Connection conn = dba.getConnection();
 		Statement stmt = conn.createStatement();
 
-	
-			if (id_user == -1) {
-				throw new Exception("id_user wrong");
-			} else {
-				String sqlQuery = "UPDATE documentos SET id_user = '" + id_user
-						+ "' WHERE id = " + id;
-				stmt.execute(sqlQuery);
-				String sqlQuerytime_update = "UPDATE documentos SET d_alteracao = '"
-						+ d_alteracao + "' WHERE id = " + id;
-				stmt.execute(sqlQuerytime_update);
-			}
-		
+
+		if (id_user == -1) {
+			throw new Exception("id_user wrong");
+		} else {
+			String sqlQuery = "UPDATE documentos SET id_user = '" + id_user
+					+ "' WHERE id = " + id;
+			stmt.execute(sqlQuery);
+			String sqlQuerytime_update = "UPDATE documentos SET d_alteracao = '"
+					+ d_alteracao + "' WHERE id = " + id;
+			stmt.execute(sqlQuerytime_update);
+		}
+
 	}
 
 
@@ -197,6 +256,10 @@ public class Documento {
 		Statement stmt = conn.createStatement();
 		String sqlQuery = "DELETE FROM documentos WHERE id = " + i;
 		stmt.execute(sqlQuery);
+	}
+	
+	public String toString(){
+		return "Título: "+this.title+"; Id do doc: "+id+"; Id do user: "+id_user+id+"; Timestamp create: "+d_criacao;
 	}
 
 }
